038933ee8c59aa411c3402e3090523ffc0c23e54
Rahul Prajapat <rahulprajapat9@users.noreply.github.com>
2017-06-18 18:43:49 +0200

    handle_communication function in gateway.c is completed
    Wrote the handle_communication function. Made two cases : Downstream when communicating with sensor node and Upstream when communicating with backend over modem.

--- src/gateway.c
+++ src/gateway.c
@@ -6,7 +6,94 @@
  * This function is polled by the main loop and should handle any packets coming
  * in over the modem or 868 MHz communication channel.
  */
+// cmd 1
+void pong (void){
+  uint8_t ack[WIRELESS_PAYLOAD_LENGTH];
+  for(uint8_t i = 0; i<32; i++){
+    ack[i]=0x01;
+  }
+  modem_enqueue_outgoing(ack,32);
+  printf("Pong sent \n");
+}
+
+// cmd 2
+void restart_controller (void)
+{
+  //((void (far *) (void)) 0x000000) ();
+  // Requires external library. Is it allowed in Kiwi firmware?
+  printf("Node_restart initialized \n");
+}
+
+
+/**
+ * This function is polled by the main loop and should handle any packets coming
+ * in over the modem or 868 MHz communication channel.
+ */
 void handle_communication(void)
 {
-  // YOUR CODE GOES HERE
+  uint8_t downstream;
+  downstream = 1;
+  if(downstream){
+    /*******from backend********/
+    printf("reading payload coming from the backend ... \n");
+    
+    uint8_t** backend_recv_data = 0;
+    size_t* len_backend_recv_data;
+    
+    modem_dequeue_incoming(backend_recv_data, len_backend_recv_data);
+    //printf("recv_data is : %X \n", backend_recv_data);
+    
+    device_id_t payload_deviceID = {0};
+    memcpy(payload_deviceID.bytes, &(&backend_recv_data)[1], 16* sizeof(uint8_t));
+    
+    printf("finished reading device id from payload as bytes and words format \n");
+    printf("Bytes[0] is : %X \n", payload_deviceID.bytes[0]);
+    printf("Words[0] is : %X \n", payload_deviceID.words[0]);
+    
+    device_id_t GW_own_dev_id = get_device_id();
+    if(payload_deviceID.bytes == GW_own_dev_id.bytes){
+      printf("my own dev \n");
+      
+      uint8_t cmd = &(&backend_recv_data)[16];
+      printf("cmd is : %d \n", cmd);
+      
+      cmd = 1;
+      switch (cmd)
+      {
+        case 1: 
+          printf("Pong() is requested by backend \n");
+          pong();
+          break;
+        case 2: 
+          printf("Restart_GW is requested by backend \n");
+          restart_controller();
+          break;
+        default: 
+          printf("Anything other than specified commands, do nothing \n");
+          break; 
+      }
+    }
+    else{
+      printf("it's sensor's dev id \n");
+      
+      uint8_t data_to_sensor[WIRELESS_PAYLOAD_LENGTH] = {0};
+      
+      memcpy(data_to_sensor, &(&backend_recv_data)[16], 17* sizeof(uint8_t));
+      wireless_enqueue_outgoing(payload_deviceID,data_to_sensor);
+    }
+    
+    //printf("reading token id from payload \n");
+    //uint8_t* token_id = &recv_data[16];    
+  }
+  else{
+    /************from sensor************/
+    device_id_t *sensor_device_id = 0;
+    uint8_t sensor_recv_data[WIRELESS_PAYLOAD_LENGTH] = {0};
+    wireless_dequeue_incoming(sensor_device_id,sensor_recv_data);
+    
+    size_t len_sensor_recv_data = strlen(sensor_recv_data);
+    printf("len_sensor_recv_data is : %d \n", len_sensor_recv_data);
+    modem_enqueue_outgoing(sensor_recv_data,len_sensor_recv_data);    
+  }
+
 }
