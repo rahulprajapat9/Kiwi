1fc3344ce9e11ba397cdefeb9e126db0edeb4194
Rahul Prajapat <rahulprajapat9@users.noreply.github.com>
2017-06-18 20:21:15 +0200

    Implemented non-blocking polling in gateway

--- src/gateway.c
+++ src/gateway.c
@@ -31,69 +31,103 @@ void restart_controller (void)
  */
 void handle_communication(void)
 {
-  uint8_t downstream;
-  downstream = 1;
-  if(downstream){
-    /*******from backend********/
-    printf("reading payload coming from the backend ... \n");
-    
-    uint8_t** backend_recv_data = 0;
-    size_t* len_backend_recv_data;
-    
-    modem_dequeue_incoming(backend_recv_data, len_backend_recv_data);
-    //printf("recv_data is : %X \n", backend_recv_data);
-    
-    device_id_t payload_deviceID = {0};
-    memcpy(payload_deviceID.bytes, &(&backend_recv_data)[1], 16* sizeof(uint8_t));
-    
-    printf("finished reading device id from payload as bytes and words format \n");
-    printf("Bytes[0] is : %X \n", payload_deviceID.bytes[0]);
-    printf("Words[0] is : %X \n", payload_deviceID.words[0]);
-    
-    device_id_t GW_own_dev_id = get_device_id();
-    if(payload_deviceID.bytes == GW_own_dev_id.bytes){
-      printf("my own dev \n");
+  uint8_t downstream=0;
+  uint8_t upstream=0;
+  //downstream = 1;
+  while(true){
+  
+      uint8_t** backend_recv_data = 0;
+      size_t* len_backend_recv_data;
       
-      uint8_t cmd = &(&backend_recv_data)[16];
-      printf("cmd is : %d \n", cmd);
       
-      cmd = 1;
-      switch (cmd)
-      {
-        case 1: 
-          printf("Pong() is requested by backend \n");
-          pong();
-          break;
-        case 2: 
-          printf("Restart_GW is requested by backend \n");
-          restart_controller();
-          break;
-        default: 
-          printf("Anything other than specified commands, do nothing \n");
-          break; 
+  
+    if(modem_dequeue_incoming(backend_recv_data, len_backend_recv_data)==1){
+      downstream = 1;
+      printf("modem is 1 \n");
+    }
+  
+    if(downstream==1){
+      /*******from backend********/
+      printf("reading payload coming from the backend ... \n");
+      
+      uint8_t** backend_recv_data = 0;
+      size_t* len_backend_recv_data;
+      
+      modem_dequeue_incoming(backend_recv_data, len_backend_recv_data);
+      //printf("recv_data is : %X \n", backend_recv_data);
+      
+      device_id_t payload_deviceID = {0};
+      memcpy(payload_deviceID.bytes, &(&backend_recv_data)[1], 16* sizeof(uint8_t));
+      
+      printf("finished reading device id from payload as bytes and words format \n");
+      printf("Bytes[0] is : %X \n", payload_deviceID.bytes[0]);
+      printf("Words[0] is : %X \n", payload_deviceID.words[0]);
+      
+      device_id_t GW_own_dev_id = get_device_id();
+      if(payload_deviceID.bytes == GW_own_dev_id.bytes){
+        printf("my own dev \n");
+        
+        //uint8_t cmd = &(&backend_recv_data)[16];
+        //printf("cmd is : %d \n", cmd);
+        
+        uint8_t* tt = *backend_recv_data[16];
+        uint8_t cmd = *tt;
+        
+        cmd = 1;
+        switch (cmd)
+        {
+          case 1: 
+            printf("Pong() is requested by backend \n");
+            pong();
+            break;
+          case 2: 
+            printf("Restart_GW is requested by backend \n");
+            restart_controller();
+            break;
+          default: 
+            printf("Anything other than specified commands, do nothing \n");
+            break; 
+        }
       }
-    }
-    else{
-      printf("it's sensor's dev id \n");
+      else{
+        printf("it's sensor's dev id \n");
+        
+        uint8_t data_to_sensor[WIRELESS_PAYLOAD_LENGTH] = {0};
+        
+        memcpy(data_to_sensor, &(&backend_recv_data)[16], 17* sizeof(uint8_t));
+        wireless_enqueue_outgoing(payload_deviceID,data_to_sensor);
+      }
       
-      uint8_t data_to_sensor[WIRELESS_PAYLOAD_LENGTH] = {0};
+      //printf("reading token id from payload \n");
+      //uint8_t* token_id = &recv_data[16];    
       
-      memcpy(data_to_sensor, &(&backend_recv_data)[16], 17* sizeof(uint8_t));
-      wireless_enqueue_outgoing(payload_deviceID,data_to_sensor);
+      downstream = 0;
+      
     }
     
-    //printf("reading token id from payload \n");
-    //uint8_t* token_id = &recv_data[16];    
-  }
-  else{
-    /************from sensor************/
     device_id_t *sensor_device_id = 0;
-    uint8_t sensor_recv_data[WIRELESS_PAYLOAD_LENGTH] = {0};
-    wireless_dequeue_incoming(sensor_device_id,sensor_recv_data);
+      uint8_t sensor_recv_data[WIRELESS_PAYLOAD_LENGTH] = {0};
+      
+    if(wireless_dequeue_incoming(sensor_device_id,sensor_recv_data)==1){
+      upstream = 1;
+      printf("sensor is 1 \n");
+    }
     
-    size_t len_sensor_recv_data = strlen(sensor_recv_data);
-    printf("len_sensor_recv_data is : %d \n", len_sensor_recv_data);
-    modem_enqueue_outgoing(sensor_recv_data,len_sensor_recv_data);    
+    if(upstream==1){
+      /************from sensor************/
+      device_id_t *sensor_device_id = 0;
+      uint8_t sensor_recv_data[WIRELESS_PAYLOAD_LENGTH] = {0};
+      wireless_dequeue_incoming(sensor_device_id,sensor_recv_data);
+      
+      size_t len_sensor_recv_data = strlen(sensor_recv_data);
+      printf("len_sensor_recv_data is : %d \n", len_sensor_recv_data);
+      modem_enqueue_outgoing(sensor_recv_data,len_sensor_recv_data);  
+      
+      upstream = 0;
+      
+    }
+  
   }
 
+  
 }
