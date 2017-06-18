e6a8153de161a847287e3daf35c8ca1ff848de49
Rahul Prajapat <rahulprajapat9@users.noreply.github.com>
2017-06-18 12:07:52 +0200

    Base Version 0.0
    Base code structure provided by Kiwi.

---
+++ src/gateway.c
@@ -0,0 +1,12 @@
+#include "gateway/modem.h"
+#include "gateway/wireless.h"
+#include "common/device.h"
+
+/**
+ * This function is polled by the main loop and should handle any packets coming
+ * in over the modem or 868 MHz communication channel.
+ */
+void handle_communication(void)
+{
+  // YOUR CODE GOES HERE
+}
