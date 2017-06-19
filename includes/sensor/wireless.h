46e9936d5640f816f26ca9786161412d9e0bb95a
Rahul Prajapat <rahulprajapat9@users.noreply.github.com>
2017-06-19 07:10:12 +0200

    cleaned up corrupted header files
    Reverting header files to original

--- includes/sensor/wireless.h
+++ includes/sensor/wireless.h
@@ -11,19 +11,10 @@
  * `*data` and returns true; if there wasn't then returns false and doesn't
  * touch `data`.
  */
-bool wireless_dequeue_incoming(uint8_t data[static WIRELESS_PAYLOAD_LENGTH]){
-  for(uint8_t i = 0; i<16; i++){
-    data[i]=0x0F;
-  }
-  for(uint8_t i = 16; i<32; i++){
-    data[i]=0xEE;
-  }
-  return true;
-}
+bool wireless_dequeue_incoming(uint8_t data[static WIRELESS_PAYLOAD_LENGTH]);
 
 /**
  * Enqueues a packet to be sent to a gateway.  The data is copied out during
  * this call so `data` can be reused as soon as this returns.
  */
-void wireless_enqueue_outgoing(uint8_t const data[static WIRELESS_PAYLOAD_LENGTH]){
-}
+void wireless_enqueue_outgoing(uint8_t const data[static WIRELESS_PAYLOAD_LENGTH]);
